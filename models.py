fa08c825cba065679ad5faada50e821d86d771f9
Pablo Huijse <pablo.huijse@gmail.com>
2020-07-22 12:57:56 -0400

    Refactor diagonal and spherical distribution to GaussianLayer, simplify StochasticAutoencoder class, improve readability, update notebook

--- models.py
+++ models.py
@@ -3,32 +3,27 @@ import torch.nn as nn
 import numpy as np
 
 
-class GaussianParameterizedNetwork(nn.Module):
-    def __init__(self, n_features, covariance_type='diag', variance_range_limiter=None):
+class GaussianLayer(nn.Module):
+    def __init__(self, input_dim, output_dim, covariance_type='diagonal', variance_range_limiter=None):
         super(type(self), self).__init__()
-        #self.covariance_type = covariance_type
         self.variance_range_limiter = variance_range_limiter
-        if covariance_type not in ['spherical', 'diag']:
-            raise ValueError("covariance_type should be in ['spherical', 'diag']")
-        if len(n_features) < 2:
-            raise ValueError("Please specify at least two layers")
-        self.hidden_layers = nn.ModuleList([nn.Linear(n_features[k], n_features[k+1])
-                                            for k in range(len(n_features)-2)])
-        self.param_mu = nn.Linear(n_features[-2], n_features[-1])
-        if covariance_type == 'diag':
-            self.param_logvar = nn.Linear(n_features[-2], n_features[-1])
+        
+        if covariance_type not in ['spherical', 'diagonal']:
+            raise ValueError("covariance_type should be in ['spherical', 'diagonal']")
+        
+        self.param_mu = nn.Linear(input_dim, output_dim)    
+        if covariance_type == 'diagonal':
+            self.param_logvar = nn.Linear(input_dim, output_dim)
         elif covariance_type == 'spherical':
-            self.param_logvar = nn.Linear(n_features[-2], 1)
-        self.activation = nn.ReLU()
-
+            self.param_logvar = nn.Linear(input_dim, 1)
+            
     def forward(self, x):
-        for layer in self.hidden_layers:
-            x = self.activation(layer(x))
         log_var = self.param_logvar(x)
         if self.variance_range_limiter is not None:
             log_var = nn.Tanh()(log_var)*self.variance_range_limiter
-        return self.param_mu(x), log_var
-    
+        return self.param_mu(x), self.param_logvar(x)
+
+   
 """ 
 class BernoulliParameterizedNetwork(torch.nn.Module):
     def __init__(self, latent_dim, output_dim, hidden_dim=128):
@@ -59,33 +54,55 @@ class _StochasticAutoencoder(nn.Module):
     def __init__(self, latent_dim, data_dim, hidden_dim=128):
         #super(type(self), self).__init__() 
         nn.Module.__init__(self)
-        self.encoder = GaussianParameterizedNetwork(n_features=[data_dim] + [hidden_dim]*2  +[latent_dim],
-                                                    covariance_type='diag')
+        
+        encoder_dim = [data_dim] + [hidden_dim]*2
+        self.encoder_network = nn.ModuleList([nn.Linear(input_dim, output_dim)
+                                              for input_dim, output_dim in zip(encoder_dim[:-1], encoder_dim[1:])])
+        
+        # Inferential distribution parameters
+        self.q_parameters = GaussianLayer(input_dim=encoder_dim[-1], output_dim=latent_dim, 
+                                          covariance_type='diagonal')
+        
+        
+        decoder_dim = [latent_dim] + [hidden_dim]*2
+        self.decoder_network = nn.ModuleList([nn.Linear(input_dim, output_dim)
+                                              for input_dim, output_dim in zip(decoder_dim[:-1], decoder_dim[1:])])
+        # Generative distribution parameters
+        self.p_parameters = GaussianLayer(input_dim=decoder_dim[-1], output_dim=data_dim, 
+                                          covariance_type='spherical')
         # TODO: variance_range_limiter, greater than 4 does not work, why?
         # Check upper/lower range reached
-        self.decoder = GaussianParameterizedNetwork(n_features=[latent_dim] + [hidden_dim]*2 + [data_dim],
-                                                    covariance_type='diag', variance_range_limiter=4)
+        self.activation = nn.ReLU()
 
+    def encode(self, x):
+        for layer in self.encoder_network:
+            x = self.activation(layer(x))
+        return self.q_parameters(x)
+        
     def sample(self, mu, std, mc_samples=1):
         batch_size, n_latent = mu.shape
         eps = torch.randn(batch_size, mc_samples, n_latent, 
                           device=std.device, requires_grad=False)
         return eps.mul(std.unsqueeze(1)).add(mu.unsqueeze(1))
 
+    def decode(self, z):
+        for layer in self.decoder_network:
+            z = self.activation(layer(z))
+        return self.p_parameters(z)
+        
     def forward(self, x, mc_samples=1):
-        z_mu, z_logvar = self.encoder(x)
-        z = self.sample(z_mu, (0.5*z_logvar).exp(), mc_samples)
-        return self.decoder(z), (z_mu, z_logvar), z
+        q_mu, q_logvar = self.encode(x)
+        z = self.sample(q_mu, (0.5*q_logvar).exp(), mc_samples)
+        return self.decode(z), (q_mu, q_logvar), z
     
     def negELBO(self, x, mc_samples=1):
-        dec_output, enc_output, z = self.forward(x, mc_samples)
-        dec_mu, dec_logvar = dec_output
-        enc_mu, enc_logvar = enc_output
-        logpxz = gaussian_log_likelihood(x, dec_mu, dec_logvar)
-        logqzxpz = self.KL_cost(enc_mu, enc_logvar, z)
+        p_parameters, q_parameters, z = self.forward(x, mc_samples)
+        p_mu, p_logvar = p_parameters
+        q_mu, q_logvar = q_parameters
+        logpxz = gaussian_log_likelihood(x, p_mu, p_logvar)
+        logqzxpz = self.KL_cost(q_mu, q_logvar, z)
         C = torch.log(torch.Tensor([mc_samples]))
         loss = torch.sum(torch.logsumexp(logqzxpz - logpxz, dim=1)) + C
-        #loss = torch.sum(logqzxpz - logpxz)
         return loss, logpxz.sum(), logqzxpz.sum()
         
 
